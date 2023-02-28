bde5e6cc6feea30388bd01b57e2455a246250487
gleb <glebcsgo23@mail.ru>
2023-02-28 20:49:29 +0300

    мой первый коммит

---
+++ main.py
@@ -0,0 +1,15 @@
+# primer = input('Введите пример: ')
+
+# print(eval(primer))
+
+# GITHUB - для сохраниний проектов
+
+# команды git
+# git init
+# git add README.md
+# git status
+# git commit -m "first commit"
+# git push -u origin main
+
+
+
